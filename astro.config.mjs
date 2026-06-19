7e9030253679219059e126ba435875442fd729fe
alfierees <alfiejrees1@gmail.com>
2026-06-19 13:20:56 +0300

    build: add Vercel adapter for hybrid rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,9 +11,12 @@ import rehypeKatex from 'rehype-katex';
 import wikiLinkPlugin from 'remark-wiki-link';
 
 import preact from '@astrojs/preact';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: vercel(),
+
   vite: {
     plugins: [tailwindcss()]
   },
